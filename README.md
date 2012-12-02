4f9316de7afaf441da4b852c41f5b332f66634fa
skaiser <freesol29@gmail.com>
2012-12-01 16:45:17 -0800

    Adding basic functionality to set CSS classes and check state

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 js-media-query
 ==============
+
+This is an alternate way to do "responsive design" that uses JavaScript and CSS
+together to determine what to do. The idea is that you set a CSS class with JS
+early in the page rendering in order to apply what you would usually put inside
+an @media media query. The reason to do this is so that you are in control of
+when to be "responsive" and not have the browser automagically respond with
+matching CSS rules, but then have the JS be "out of sync" with the CSS.
+
+I've found that many times, we only need to be "responsive" about the
+layout/design at page load and that only developers and designers are resizing
+the browser to test their responsive designs. I've also found using Modernizr.MQ
+and/or matchMedia.js to potentially cause window resize events to fire on
+Androids, which caused undesirable things to happen.
+
+This is an experiment to see if this gives more predictable control to the
+developers. This should also give us a better way to implement responsive
+design in IE (if you really are in to self torture).
+
+
+
+For example, instead of doing this:
+
+    @media only screen and (max-width: 63em) { 
+        .someStyle {
+            color: blue;
+        }
+    }
+
+    @media only screen and (max-width: 45em) {
+        .someStyle {
+            color: red;
+        }
+    }
+    
+We could just do this:
+
+    .large {
+        color: blue;
+    }
+    .medium {
+        color: red;
+    }
+    
+If you really want to update the CSS class after a window resize event, you
+can use:
+    
+    jsmq.update();
+    
+API:
+
+    jsmq.VERSION;       // Returns version info
+    jsmq.init();        // Makes things happen
+    jsmq.update();      // Refreshes the current CSS class, useful after a resize
+    jsmq.getState();    // Basically does a JS Media Query (like a CSS media query)    
+
+
+Thanks!
---
+++ js-media-query.js
@@ -0,0 +1,141 @@
+/**!
+ *  js-media-query
+ *  https://github.com/skaiser/js-media-query
+ 
+ *  License: MIT License (MIT)
+ *  Copyright (c) 2012 Stephen Kaiser
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
+
+(function (window, document, undefined) {
+    
+    var _jsmq,
+        docEl,
+        head = document.getElementsByTagName('head')[0];
+        
+    if (window.jsmq !== undefined) {
+        return;
+    }
+    
+    if (document && document.documentElement) {
+        docEl = document.documentElement;
+    } else {
+        // Not in a browser?
+        return;
+    }
+    
+    
+    var _jsmq = (function () {
+        
+        
+        // Add hidden elements to page include styles for hiding elems naturally
+        // Check their width
+        // Add CSS class
+        var VERSION = '0.0.1',
+            prevClass = '',
+            cfg = {};
+        
+        cfg.sizes = {
+            '64': 'large',
+            '63': 'medium',
+            '45': 'small',
+            '30': 'smaller'
+        };
+        
+        cfg.elemNames = {
+            'viewport': 'mediaWidth',       // Viewport/browsesr width
+            'device': 'mediaDeviceWidth'    // Width of actual device
+        }
+        
+        cfg.useDeviceWidth = false;
+        
+        
+        // getElementById
+        function _get(id) {
+            return document.getElementById(id);
+        }
+
+        function _getMediaWidth () {
+            var cs = window.getComputedStyle;
+            return cs   ? parseInt(cs(_get(cfg.elemNames['viewport'])).getPropertyValue('width'), 10)
+                        : false;
+        }
+
+        function _getMediaDeviceWidth() {
+            var cs = window.getComputedStyle;
+            return cs   ? parseInt(cs(_get(cfg.elemNames['device'])).getPropertyValue('width'), 10)
+                        : false;
+        }
+
+        function _createElem(name) {
+            var el = document.createElement('script'),
+                id = cfg.elemNames[name];
+            el.setAttribute('id', id);
+            return el;
+        }
+        
+        function _addHiddenElems() {
+            head.appendChild(_createElem('viewport'));
+            head.appendChild(_createElem('device'));
+        }
+        
+        function _setCssClass() {
+            var currClass = getState();
+            docEl.className = (" " + docEl.className + " ").replace(prevClass, " " + currClass);
+            prevClass = currClass;
+        }
+        
+        // Does a fresh check of current state
+        function update() {
+            _setCssClass();
+        }
+        
+        // Get current state
+        function getState(useDeviceWidth) {
+            var fn = useDeviceWidth ? _getMediaDeviceWidth : _getMediaWidth;
+            return cfg.sizes[fn()];
+        }
+        
+        function init(config) {
+            //_configure(config);
+            _addHiddenElems();
+            update();
+            
+        }
+        
+        
+        
+        // Public methods
+        return {
+            VERSION: VERSION,
+            update: update,
+            getState: getState,
+            init: init
+        };
+        
+    })();
+    
+    
+    
+    // Export
+    window.jsmq = _jsmq;
+    
+    window.jsmq.init();
+    
+})(this, this.document);
